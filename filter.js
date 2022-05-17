4926f0e88490f8236903a4f2b705f0d35fd1044e
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-17 18:01:41 +0300

    Release 2: done

--- filter.js
+++ filter.js
@@ -1,3 +1,4 @@
+/*
 function oddNumbers(nums) {
   let odds = []
   for (let i = 0; i < nums.length; i++) {
@@ -7,6 +8,17 @@ function oddNumbers(nums) {
   }
   return odds
 }
+console.log(oddNumbers([15, 12, 26, 236, 87]))
+
+let nums = [15, 12, 26, 236, 87];
+function getOdd(num){
+  return num % 2 === 1;
+}
+console.log(nums.filter(getOdd))
+
+let result = nums.filter((el) =>{return el %  2 ===1} )
+console.log(result)
+*/
 
 
 function longStrings(strings, minimumLength = 0){
@@ -17,3 +29,11 @@ function longStrings(strings, minimumLength = 0){
   }
   return longs
 }
+
+console.log(longStrings(["lem", "papa", "apple", "cherry"],2));
+
+const minimumLength = 2;
+const strings = ["lem", "papa", "apple", "cherry"];
+
+let result = strings.filter((elem)=>elem.length >= minimumLength)
+console.log(result);
