2087e48ba78aa0886bd4d64d52ac585a14991a11
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-17 17:18:47 +0300

    Release 2: done

--- find.js
+++ find.js
@@ -1,9 +1,15 @@
-function firstUnder(numbers, limit){
+/*function firstUnder(numbers, limit){
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] <= limit)
     return numbers[i]
   }
 }
+*/
+// console.log(firstUnder(, 10))
+
+const arr=[15, 12, 26, 236, 86];
+const limit=14;
+// console.log(arr.find((el)=> el <= limit))
 
 function startsWith(strings, letter){
   for (let i = 0; i < strings.length; i++) {
@@ -11,3 +17,8 @@ function startsWith(strings, letter){
     return strings[i]
   }
 }
+const strings = ["lemon", "papaya", "apple", "cherry"];
+const letter = 'c';
+// console.log(startsWith(strings, letter));
+
+// console.log(strings.find((el)=> el[0] === letter))
