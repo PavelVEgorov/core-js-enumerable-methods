097e5a89b5b099a3988af8e1cd29283cce4ee0ef
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-17 18:33:15 +0300

    Release 4: done

--- aggregate.js
+++ aggregate.js
@@ -1,6 +1,18 @@
 function sum(arrayNumbers, sum = 0){
   for (let i = 0; i < arrayNumbers.length; i++ ){
     sum += arrayNumbers[i];
+    // console.log(undefined);
   }
   return sum
 }
+
+console.log(sum([1, 2, 3, 4]))
+
+const summer = [1, 2, 3, 4];
+
+const result = summer.reduce(function(counts, number) {
+	counts += number;
+	return counts;
+}, 0)
+
+// console.log(result);
