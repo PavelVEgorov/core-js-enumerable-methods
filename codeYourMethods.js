85f68972d37505bdabf442e89e7e94be87e923d1
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-17 16:50:56 +0300

    Release 0: done

--- codeYourMethods.js
+++ codeYourMethods.js
@@ -4,31 +4,62 @@
 // const newArr = myMap([1,2,3], (el) => el * 2);
 
 
-function myForEach() {
-
+function myForEach(el) {
+  for(let i = 0; i < el.length; i++){
+    console.log(el[i])
+  }  
 }
+// console.log(myForEach([12, 15, 26, 236, 86]))
 
-function myMap() {
-
+function myMap(el) {
+  let newArr= []
+  for(let i = 0; i < el.length; i++){
+    newArr.push(el[i]*2)
+  }
+  return  newArr; 
 }
+// console.log(myMap([12, 15, 26, 236, 86]))
 
-function myFilter() {
-
+function myFilter(el) {
+  let newArr= []
+  for(let i = 0; i < el.length; i++){
+    if(el[i]%2 === 0) newArr.push(el[i])
+  }
+  return  newArr; 
 }
+// console.log(myFilter([12, 15, 26, 236, 86]))
 
-function myReduce() {
-
+function myReduce(el) {
+  let sum = 0;
+  for(let i = 0; i < el.length; i++){
+    if(el[i]%2 === 0) 
+    sum+=el[i];
+  }
+  return  sum;
 }
+// console.log(myReduce([12, 15, 26, 236, 86]))
 
-function myFind() {
-
+function myFind(el) {
+  for(let i = 0; i < el.length; i++){
+    if (el[i] % 5 === 0)
+    return el[i]; 
+  }
 }
+// console.log(myFind([12, 15, 26, 86]))
 
-function myEvery() {
-
+function myEvery(el) {
+  for(let i of el){
+    if(i % 2 !== 0) return false;
+  }
+  return true
 }
+// console.log(myEvery([12, 15, 26, 236, 86]))
 
-function mySome() {
-
+function mySome(el) {
+  for(let i of el){
+    if(typeof(i) !== 'number') return true;
+  } 
+  return false;
 }
+console.log(mySome([12, 15, 26, '236', 86]))
 
